adf736bcf54db92d6ce41c31f155702a32e834ff
da-yooom <kdh941110@gmail.com>
2022-01-11 00:54:26 +0900

    ✅ 로딩 상태  추가

--- src/components/Meals/AvaliableMeals.js
+++ src/components/Meals/AvaliableMeals.js
@@ -5,6 +5,7 @@ import MealItem from './MealItem/MealItem';
 
 const AvaliableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchMeals = async () => {
@@ -25,10 +26,19 @@ const AvaliableMeals = () => {
       }
 
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
 
     fetchMeals();
   }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
 
   const mealsList = meals.map((meal) => (
     <MealItem
